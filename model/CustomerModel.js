42dcb58f6bcd3ddd962f0e29a30594c03224781f
tesikaasemi2003 <asemitesika68@gmail.com>
2026-05-02 01:30:31 +0530

    first commit

--- model/CustomerModel.js
+++ model/CustomerModel.js
@@ -1,2 +1,13 @@
 import { customer_db } from 'db/db.js';
 import * as db from 'db/db.js';
+
+class Customer {
+    #id;
+    #name;
+    #phone;
+    #email;
+    #address;
+    #loyaltyPoints;
+    #totalSpent;
+    #joinDate;
+}
