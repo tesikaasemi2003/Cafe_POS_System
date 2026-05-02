952d200bd1e4fda2bb1bab68ae6dff1a96bd7002
tesikaasemi2003 <asemitesika68@gmail.com>
2026-05-02 11:56:55 +0530

    first commit

--- model/CustomerModel.js
+++ model/CustomerModel.js
@@ -10,4 +10,32 @@ class Customer {
     #loyaltyPoints;
     #totalSpent;
     #joinDate;
+
+    constructor(id, name, phone, email = '', address = '', loyaltyPoints = 0, totalSpent = 0) {
+        this.#id = id;
+        this.#name = name;
+        this.#phone = phone;
+        this.#email = email;
+        this.#address = address;
+        this.#loyaltyPoints = loyaltyPoints;
+        this.#totalSpent = totalSpent;
+        this.#joinDate = new Date().toISOString();
+
+    }
+    get id()            { return this.#id; }
+    get name()          { return this.#name; }
+    get phone()         { return this.#phone; }
+    get email()         { return this.#email; }
+    get address()       { return this.#address; }
+    get loyaltyPoints() { return this.#loyaltyPoints; }
+    get totalSpent()    { return this.#totalSpent; }
+    get joinDate()      { return this.#joinDate; }
+
+    set name(name)                   { this.#name = name; }
+    set phone(phone)                 { this.#phone = phone; }
+    set email(email)                 { this.#email = email; }
+    set address(address)             { this.#address = address; }
+    set loyaltyPoints(loyaltyPoints) { this.#loyaltyPoints = loyaltyPoints; }
+    set totalSpent(totalSpent)       { this.#totalSpent = totalSpent; }
 }
+
